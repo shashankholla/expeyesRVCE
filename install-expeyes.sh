7ee3835f8257dd092e9f4008f19009401abb4248
shashankholla <shashu1999@gmail.com>
2018-07-28 21:44:03 +0530

    Fixed run.sh

--- install-expeyes.sh
+++ install-expeyes.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+sudo apt-get install python3-pip
+
+
 cd ~/Documents
+wget https://github.com/shashankholla/expeyesRVCE/raw/master/requirements.txt
+pip3 install -r requirements.txt
 wget https://github.com/shashankholla/expeyesRVCE/raw/master/expeyesRVCE-0.0.1.deb
 dpkg -i expeyesRVCE-0.0.1.deb
 rm -f expeyesRVCE-0.0.1.deb
