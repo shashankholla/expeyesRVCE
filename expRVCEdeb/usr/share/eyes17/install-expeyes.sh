916d27808da8425cc9cbd41ff7e91a3c988a18c9
shashankholla <shashu1999@gmail.com>
2018-07-28 21:58:04 +0530

    rebuilt deb

--- expRVCEdeb/usr/share/eyes17/install-expeyes.sh
+++ expRVCEdeb/usr/share/eyes17/install-expeyes.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-cd ~/Documents
-mkdir expeyes
-cd expeyes
-wget https://github.com/shashankholla/expeyesRVCE/archive/master.zip
-unzip expeyesRVCE-master.zip
-cd expeyesRVCE-master
 sudo apt-get install python3-pip
-pip install -r requirements.txt
-cp ExpeyesRVCE.desktop ~/.local/share/applications
-cp eyes17-logo.png ~/.local/share/icons
+
+
+cd ~/Documents
+wget https://github.com/shashankholla/expeyesRVCE/raw/master/requirements.txt
+pip3 install -r requirements.txt
+wget https://github.com/shashankholla/expeyesRVCE/raw/master/expeyesRVCE-0.0.1.deb
+dpkg -i expeyesRVCE-0.0.1.deb
+rm -f expeyesRVCE-0.0.1.deb
+apt-get install -f
 
 
 
