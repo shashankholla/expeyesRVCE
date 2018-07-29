ad0cf703d7028f2ac79b2cc2013d02d9e5780190
shashankholla <shashu1999@gmail.com>
2018-07-29 13:22:26 +0530

    Update install-expeyes.sh

--- install-expeyes.sh
+++ install-expeyes.sh
@@ -6,7 +6,7 @@ cd ~/Documents
 wget https://github.com/shashankholla/expeyesRVCE/raw/master/requirements.txt
 pip3 install -r requirements.txt
 wget https://github.com/shashankholla/expeyesRVCE/raw/master/expeyesRVCE-0.0.1.deb
-dpkg -i expeyesRVCE-0.0.1.deb
+dpkg -i --force-overwrite expeyesRVCE-0.0.1.deb
 rm -f expeyesRVCE-0.0.1.deb
 rm -f requirements.txt
 apt-get -y install -f
